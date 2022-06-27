ffea3b8abd4a86006132024262ece177721a535b
Vyacheslav <vaceslavcertopolohov@gmail.com>
2022-06-27 18:41:31 +0300

    add seminars

---
+++ MySeminars/Seminar1/Program.cs
@@ -0,0 +1,55 @@
+﻿
+//int a; 
+//double b = 5.0;
+//string c = "Slavik";
+//int num = 5; 
+
+//num = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("My number is" + num);
+
+ //задача 1
+
+/*
+double num;
+
+Console.Write("Введи любое число ");
+
+num = Convert.ToDouble(Console.ReadLine());
+
+if(num == 1)
+{
+    Console.Write("Это понедельник");
+}
+if(num == 2)
+{
+    Console.Write("Это вторник");
+}
+if(num == 3)
+{
+    Console.Write("Это среда");
+}
+else
+{
+    Console.Write("Не верное число");
+}
+( || )
+*,
+/*
+int num, a;
+
+Console.Write("Введи любое число ");
+num = Convert.ToInt32(Console.ReadLine());
+
+a = num * num;
+
+Console.Write(a);
+*/
+
+///
+
+
+
+
+
+
