a025c11d5b1e1eb472ef943003ab985c72f30854
Vyacheslav <vaceslavcertopolohov@gmail.com>
2022-06-27 21:35:43 +0300

    Задача 1

--- MyHomework/Homework1/Program.cs
+++ MyHomework/Homework1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int num1;
+int num2;
+
+Console.WriteLine("Введи два числа. ");
+num1 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("число " + num1 + " больше числа " + num2);
+}
+if(num1 == num2)
+{
+    Console.WriteLine("число " + num1 + " число " + num2 + " равны");
+}
+else
+{
+    Console.WriteLine("число " + num1 + " меньше числа " + num2);
+}
+
