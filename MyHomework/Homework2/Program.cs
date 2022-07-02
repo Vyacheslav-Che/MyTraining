d1d673f0926f0471f99ecffa190179519e569f84
Vyacheslav <vaceslavcertopolohov@gmail.com>
2022-07-02 16:09:51 +0300

    Homeworc 2

---
+++ MyHomework/Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Домашнее задание 2.
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void OutputSecondNum(int num)
+{   
+    int res; 
+    if(num > 100 && num < 1000)
+    {
+        res =  num / 10;
+        res = res % 10; 
+        Console.WriteLine("Вторая цифра числа " + res);
+    }
+    else
+    {
+         Console.WriteLine("Не верное число");
+    }
+
+}
+
+Console.Write("Введите трехзначное чиссло: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+OutputSecondNum(num2);
+
