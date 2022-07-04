05c5e1376596f51d65b21f0f6921cae19ab3b964
Vyacheslav <vaceslavcertopolohov@gmail.com>
2022-07-04 19:06:26 +0300

    решил первую задачу в домашней работе 3

---
+++ MyHomework/Homework3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void ChecPalindrom(int num)
+{
+   int a = num;
+   int b = 0;
+   int c = 0; 
+
+   while (num > 0)
+   {
+        c = num % 10;
+        b = b * 10 + c; 
+        num = num / 10;
+   }
+
+    if(a == b)
+    {
+        Console.Write("Число являеться палиндромом");
+    }
+    else
+    {
+        Console.Write("Число не являеться палиндромом");
+    }
+
+   
+}
+
+Console.Write("Введите пятизначное число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ChecPalindrom(num2);
+
